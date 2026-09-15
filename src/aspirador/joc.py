826b7e231670f697584ab5676d0933512e94356c
Bartomeu Miró Mateu <bartomeu.miro@uib.cat>
2026-09-15 13:12:49 +0200

    raw untested code for first session

--- src/aspirador/joc.py
+++ src/aspirador/joc.py
@@ -5,21 +5,39 @@ class Aspirador(joc.JocNoGrafic):
     def __init__(self, agents: list[agent.Agent] | None = None):
         if agents is None:
             agents = []
-        super(Aspirador, self).__init__(agents=agents)
-        # TODO
 
+        super(Aspirador, self).__init__(agents=agents)
+        self.position = "a"
+        self.rooms = {
+            "a": {
+                "dirty": True
+             }
+            "b": {
+                "dirty": True
+            }
+        }
 
     def _draw(self):
-        # TODO
-        pass
-
+        for room, status in self.rooms.items():
+            status = "dirty" if status[dirty] else "clean"
+            print(f"Room {room} is {status}")
 
     def percepcio(self):
-        # TODO
-        pass
-
+        return {
+            "position": self.positon
+            "status": self.rooms[self.position]
+        }
 
     def _aplica(self, accio, params=None, agent_actual=None):
-        # TODO
-        pass
 
+        if accio == "clean":
+            self.rooms[self.position]["dirty"] = False
+
+        elif accio == "move":
+            self.position = params["position"]
+
+        elif accio == "check":
+            for status in self.rooms.values():
+                if status["dirty"]:
+                    return False
+            return True
